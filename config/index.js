ce343dec45e3dd051af9d09a7cf9524beb858039
Sarisia <sarisiaice@gmail.com>
2019-08-08 09:48:39 +0900

    これマジ？

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
